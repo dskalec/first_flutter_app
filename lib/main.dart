473b93005ff5454f5c64f3a0713f5ef0be7ce99c
Davor Škalec <davor.skalec@styria.dev>
2020-12-07 22:45:30 +0100

    First flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+// inheritance using extends
+class MyApp extends StatelessWidget {
+  // Widget is the return type of the build method
+  // BuildContext is the type of the context argument
+  Widget build(BuildContext context) {
+    // MaterialApp is of type Widget (extends StatefulWidget)
+    return MaterialApp(
+      home: Text('Hello World!'),
+    );
+  }
+}
