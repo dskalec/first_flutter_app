2a5d325333f06af325f9a49abf4f78d16cb93a3d
Davor Škalec <davor.skalec@styria.dev>
2020-12-13 01:29:36 +0100

    Extract Question as a separate widget - video 39 finished.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,65 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import './question.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 // inheritance using extends
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  // _ turns it into a private class
+  var _currentQuestion = 0;
+  void answerQuestion() {
+    setState(() {
+      _currentQuestion++;
+    });
+    print(_currentQuestion);
+  }
+
   // Widget is the return type of the build method
   // BuildContext is the type of the context argument
+  @override // makes the code cleaner -> makes it clear that we're overriding it
   Widget build(BuildContext context) {
     // MaterialApp is of type Widget (extends StatefulWidget)
+    var questions = [
+      'What\'s your favourite colour?',
+      'What\'s your favourite animal?',
+      'What\'s your favourite music band?'
+    ];
+
     return MaterialApp(
-      home: Text('Hello World!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Foodness'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Question(questions[_currentQuestion]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            )
+          ],
+        ), // <Widget> specifies type of list items, moze i bez tog
+      ),
     );
   }
 }
