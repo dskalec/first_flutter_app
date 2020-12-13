ea050cfd8fdbd0c4689ab0bb29bb5e2cf4b500d7
Davor Škalec <davor.skalec@styria.dev>
2020-12-13 23:26:33 +0100

    Extract Answer as a separate widget - video 46 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import './answer.dart';
 
 // void main() {
 //   runApp(MyApp());
@@ -31,32 +32,34 @@ class _MyAppState extends State<MyApp> {
   @override // makes the code cleaner -> makes it clear that we're overriding it
   Widget build(BuildContext context) {
     // MaterialApp is of type Widget (extends StatefulWidget)
-    var questions = [
-      'What\'s your favourite colour?',
-      'What\'s your favourite animal?',
-      'What\'s your favourite music band?'
+    const questions = [
+      // in Dart this is called a Map, not a dictionary
+      {
+        'questionText': 'What\'s your favourite colour?',
+        'answers': ['Black', 'White', 'Red', 'None of the above'],
+      },
+      {
+        'questionText': 'What\'s your favourite animal?',
+        'answers': ['Cat', 'Dog', 'Parrot', 'None of the above'],
+      },
+      {
+        'questionText': 'What\'s your favourite music band?',
+        'answers': ['Nickelback', 'Daughtry', 'PoTF', 'None of the above'],
+      },
     ];
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Foodness'),
+          title: Text('My first app'),
         ),
         body: Column(
           children: <Widget>[
-            Question(questions[_currentQuestion]),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: answerQuestion,
-            )
+            Question(questions[_currentQuestion]['questionText']),
+            ...(questions[_currentQuestion]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(answerQuestion, answer);
+            }).toList(),
           ],
         ), // <Widget> specifies type of list items, moze i bez tog
       ),
--- lib/question.dart
+++ lib/question.dart
@@ -7,6 +7,14 @@ class Question extends StatelessWidget {
   Question(this.questionText);
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(15),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 22),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
